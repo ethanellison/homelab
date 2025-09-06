ca93e63f368dee4001c3afcc1c11e28713f074ca
eellisonarch <contact@sneakysquid.xyz>
2025-09-06 12:12:09 -0400

    updated scripts and added script to deploy stack on host

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -49,7 +49,7 @@ install_portainer() {
   echo -e "${GREEN}Installing Portainer...${NC}"
   docker volume create portainer_data
   docker run -d \
-    -p 8000:8000 -p 9443:9443 \
+    -p 8000:8000 -p 9443:9443 -p 9000:9000 \
     --name portainer \
     --restart=always \
     -v /var/run/docker.sock:/var/run/docker.sock \
